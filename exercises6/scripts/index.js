1c500efc6e815fe4f020519313004d6427cb6075
Simon Appeltofft <31758722+VonKaffe@users.noreply.github.com>
2018-02-04 14:55:20 +0100

    Exercise 6

---
+++ exercises6/scripts/index.js
@@ -0,0 +1,15 @@
+
+var name = prompt("Enter your name here:")
+
+var lname = prompt("Enter your name here:")
+
+
+
+function splitString(splitToSplit) {
+name.toLowerCase();
+lname.toUpperCase();
+
+}
+
+console.log(name.toLowerCase());
+console.log(lname.toUpperCase());
